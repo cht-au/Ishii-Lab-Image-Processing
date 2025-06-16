85cf7d1db948072be11e3ea9eceb512fc5890314
cht-au <145236495+cht-au@users.noreply.github.com>
2025-06-16 18:00:34 +0900

    make functions to contain steps

--- videoTesting.m
+++ videoTesting.m
@@ -1,75 +1,77 @@
 clc;
 clear;
 
-
 vid = VideoReader("Ishii Lab Project Video 2025.mp4");
 vid.CurrentTime = 60; % skipping 
+fishElem = strel('disk', 9); % TODO: Adjust value 
+hBlobAnalysis = vision.BlobAnalysis('MinimumBlobArea',3500,...
+        'MaximumBlobArea',60000);
+
 while hasFrame(vid)
     frame = readFrame(vid);
+    frame = imrotate(frame, 270);
+    frame = imcrop(frame,[0 200 825 1200]);
     %% Displaying 
-    % used later 
     set(gcf, 'Position', get(0, 'Screensize'));
     figure(1)    
-    tiledlayout(2,2,"TileSpacing","none");
-
-
+    tiledlayout(1,4,"TileSpacing","none");   
     %% Image Pre-Processing 
-    I = frame;
+    preimage = Preprocess(frame);
+    inpainted = RemoveNet(preimage);
+    %% Get fish
+    [annotated,bw_inverted,removedDisturb] = extractFish(inpainted,fishElem,hBlobAnalysis,preimage);
+    %% Displaying
 
-    %TODO: Find if this function is better
-    %I2 = imreducehaze(I2);
-    
-    %%Original cropping
-    I2 = imcrop(I,[100 100 1400 1500]);
-    %%Modified cropping
-    %I2 = imcrop(imrotate(I, 270),[0 200 825 1200]); % Modffed cropped and modified
-    I3 = rgb2gray(I2);
-    I4 = imadjust(I3);
-    I5 = im2uint8(I4);
-    I6 = adapthisteq(I5);
-    I7 = imsharpen(I6);
+    nexttile
+    imshow(annotated);
+
+     nexttile
+    imshow(bw_inverted);
+
+    nexttile
+    imshow(removedDisturb);
+end
 
 
-    %% Net Removal
-    edges = edge(I6, 'Canny');
+function inpainted = RemoveNet(img)
+    edges = edge(img, 'Canny');
     strelLength = 11;
     se1 = strel('line', strelLength, 90);
     se2 = strel('line', strelLength, 0);
     se3 = strel('line', strelLength, 45);
     se4 = strel('line', strelLength, 135);
     netMask = imdilate(edges, se1) | imdilate(edges, se2) | imdilate(edges, se3) | imdilate(edges, se4);
-    inpainted = regionfill(I6, netMask);
-    %% Readjustment
-    %inpainted = imadjust(inpainted);
-    inpainted = imsharpen(inpainted); % might be unnesscary
-    %% Thresholding
-    bw = imbinarize(inpainted,"adaptive","Sensitivity",0.70);
-    bw_inverted = ~bw;
-    %% Remove disturbances - 
-    fishElem = strel('diamond', 10); % TODO: Adjust value 
-    Ibwopen = imopen(bw_inverted,fishElem);
-    %% Blob Analysis
-    hBlobAnalysis = vision.BlobAnalysis('MinimumBlobArea',4300,...
-        'MaximumBlobArea',60000); % TODO: Adjust value
-    [objArea, objCentroid, bboxOut] = step(hBlobAnalysis,Ibwopen);
-    %% Annotate
-    Ishape = insertShape(I6,'rectangle',bboxOut,'Linewidth',4);
-    %% Display    
-
-    nexttile;
-    imshow(inpainted);
-    title('Net Removal');
-
-    nexttile;
-    imshow(Ishape);
-    title('Annotated Frame');
-
-    nexttile;
-    imshow(bw_inverted);
-    title('After Thresholding');
-    
-    
-
+    inpainted = regionfill(img, netMask);
 end
+
+function result = Preprocess(img)
+    %Original cropping
+    I2 = imcrop(img,[100 100 1400 1500]);
+    %Modified cropping + rotated
+    %I2 = imcrop(imrotate(I, 270),[0 200 825 1200]);
+    I3 = rgb2gray(I2);
+    I4 = imadjust(I3);
+    I5 = im2uint8(I4);
+    I6 = adapthisteq(I5);
+    result = imsharpen(I6);
+end
+
+function [annotated,bw_inverted,Ibwopen] = extractFish(inpainted,fishElem,hBlobAnalysis,originalPic)
+    image = inpainted;
+    %image = imsharpen(inpainted); % TODO: might be unnesscary
+    % Thresholding
+    bw = imbinarize(image,"adaptive","Sensitivity",0.65);
+    bw_inverted = ~bw;
+    % Remove disturbances
+    Ibwopen = imopen(bw_inverted,fishElem);
+    % Blob Analysis
+    [objArea, objCentroid, bboxOut] = step(hBlobAnalysis,Ibwopen);
+    % Annotate
+    annotated = insertShape(originalPic,'rectangle',bboxOut,'Linewidth',4);
+end
+
+
+
+
 
 
