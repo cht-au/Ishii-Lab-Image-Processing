baed35def179cecfe9e74daaa77daa8fb9dbcf71
cht-au <145236495+cht-au@users.noreply.github.com>
2025-06-25 13:18:45 +0900

    Added velocity tracking and testing

--- pictureTesting.m
+++ pictureTesting.m
@@ -3,14 +3,17 @@ clear;
 
 %% Image Preprocessing
 I = imread('fish.png');
-I2 = imcrop(I,[100 100 1400 1500]);
+I2 = imrotate(I, 270);
+I2 = imcrop(I2,[0 138.5 824 1422]);
+% Original cropping
+% I2 = imcrop(I,[100 100 1400 1500]);
 I3 = rgb2gray(I2);
 I4 = imadjust(I3);
 I5 = im2uint8(I4);
 I6 = adapthisteq(I5);
 I7 = imsharpen(I6);
-
-edges = edge(I6, 'Canny');
+I8 = medfilt2(I7);
+edges = edge(I8, 'Canny');
 
 %% Net Removal
 strelLength = 11;
@@ -19,30 +22,23 @@ se2 = strel('line', strelLength, 0);
 se3 = strel('line', strelLength, 45);
 se4 = strel('line', strelLength, 135);
 netMask = imdilate(edges, se1) | imdilate(edges, se2) | imdilate(edges, se3) | imdilate(edges, se4);
-inpainted = regionfill(I6, netMask); 
+inpainted = regionfill(I2, netMask); 
 
 imshow(inpainted)
-% 
-% 
-% 
 % %% Thresholding
 % sharpIn = imsharpen(inpainted); % TODO: Might be better to enhace image further
 % % or remove it if it is better
 % bw = imbinarize(sharpIn,"adaptive","Sensitivity",0.70); 
 % bw_inverted = ~bw;
-% % subplot(2,2,1); imshow(bw_inverted);
-% % %% Remove disturbances
+% imshow(bw_inverted)
+
+% %% Remove disturbances
 % fishElem = strel('diamond', 10);
 % Ibwopen = imopen(bw_inverted,fishElem);
-% imshow(Ibwopen)
-% 
-% % %% Blob Analysis
+% %% Blob Analysis
 % hBlobAnalysis = vision.BlobAnalysis('MinimumBlobArea',4000,...
 %     'MaximumBlobArea',10000);
 % [objArea, objCentroid, bboxOut] = step(hBlobAnalysis,Ibwopen);
-% % %% Annotate
+% %% Annotate
 % Ishape = insertShape(I2,'rectangle',bboxOut,'Linewidth',4);
 % imshow(Ishape)
-
-
-
--- videoTesting.m
+++ videoTesting.m
@@ -1,37 +1,116 @@
 clc;
 clear;
+clf;
 
 vid = VideoReader("Ishii Lab Project Video 2025.mp4");
-vid.CurrentTime = 60; % skipping 
-fishElem = strel('disk', 9); % TODO: Adjust value 
-hBlobAnalysis = vision.BlobAnalysis('MinimumBlobArea',3500,...
-        'MaximumBlobArea',60000);
+skipTime = 60;
+vid.CurrentTime = skipTime; % skipping 
 
-while hasFrame(vid)
+%% Constant for blob analysis
+fishElem = strel('disk', 10); % TODO: Adjust value 
+hBlobAnalysis = vision.BlobAnalysis('MinimumBlobArea',3500,...
+        'MaximumBlobArea',18000); % TODO: Adjust value
+
+% each row: [ID, x,y,vx, vy, frames]
+knownFish = []; 
+nextFishID = 1;
+maxDist = 125;
+maxPredictionError = 100; 
+savedFish = [];
+while vid.CurrentTime < skipTime + 0.75
     frame = readFrame(vid);
     frame = imrotate(frame, 270);
-    frame = imcrop(frame,[0 200 825 1200]);
+    frame = imcrop(frame,[0 160 824 1422]);
     %% Displaying 
     set(gcf, 'Position', get(0, 'Screensize'));
     figure(1)    
-    tiledlayout(1,4,"TileSpacing","none");   
+    tiledlayout(1,3);   
     %% Image Pre-Processing 
     preimage = Preprocess(frame);
     inpainted = RemoveNet(preimage);
     %% Get fish
-    [annotated,bw_inverted,removedDisturb] = extractFish(inpainted,fishElem,hBlobAnalysis,preimage);
+    [annotated,bw_inverted,removedDisturb,objCentroid] = extractFish(inpainted,fishElem,hBlobAnalysis,preimage);
+    %% Keeping track of fishes
+    updatedFish = []; %[ID, x, y, vx, vy]
+    % loop through the seen fish in the current frame
+    for i = 1:size(objCentroid, 1)
+        currCentroid = objCentroid(i,:); % [x, y]
+        matched = false;
+        % loop through current known fish
+        for j = 1:size(knownFish,1)
+            % if the fish is already matched
+            if knownFish(j,1) == -1
+                continue;
+            end
+            prevCentroid = knownFish(j,2:3); % current KNOWN fish 
+            velocity = knownFish(j,4:5);
+            hasVelocity = any(velocity);
+            dist = norm(currCentroid - prevCentroid);
+    
+            if hasVelocity
+                predictedPos = prevCentroid + velocity;
+                predictedDist = norm(predictedPos - currCentroid);
+                if predictedDist < maxPredictionError
+                    matched = true;
+                end
+            elseif dist < maxDist
+                matched = true;
+            end
+
+            if matched
+                % calculate velocity
+                vx = currCentroid(1) - prevCentroid(1);
+                vy = currCentroid(2) - prevCentroid(2);
+                % add the matched fish to the list with new velocities and
+                % coordinates
+                updatedFish(end+1,:) = [knownFish(j,1), currCentroid, vx, vy,knownFish(j,6)+1];
+                % marking the fish invalid
+                knownFish(j,1) = -1;
+                break;
+            end
+        end
+    
+        if ~matched
+            updatedFish(end+1,:) = [nextFishID, currCentroid, 0, 0,1];
+            nextFishID = nextFishID + 1;
+        end
+    end
+    knownFish = updatedFish;
+
+    for k = 1:size(knownFish,1)
+        speed = norm(knownFish(k,4:5));
+        id = knownFish(k,1);
+        frames = knownFish(k,6);
+        txt = sprintf('%d,%d',id,frames);
+        if frames >= 3
+            annotated = insertText(annotated, knownFish(k,2:3), txt,FontSize=38,FontColor="red");
+            savedFish(end+1,:) = id;
+        else
+            annotated = insertText(annotated, knownFish(k,2:3), txt,FontSize=38);
+        end
+        %baseFileName =sprintf('frame_%04d.png',round(vid.CurrentTime * vid.FrameRate));
+        %fullFileName = fullfile(cd,'image', baseFileName);  
+        %imwrite(annotated, fullFileName);
+    end
     %% Displaying
-
     nexttile
-    imshow(annotated);
-
-     nexttile
     imshow(bw_inverted);
-
     nexttile
     imshow(removedDisturb);
+    nexttile
+    imshow(annotated)
 end
+disp(unique(savedFish))
 
+function I8 = Preprocess(img)
+    I3 = rgb2gray(img);
+    I4 = imadjust(I3);
+    I5 = im2uint8(I4);
+    I6 = adapthisteq(I5);
+    I7 = imsharpen(I6);
+    I8 = medfilt2(I7);
+
+end
 
 function inpainted = RemoveNet(img)
     edges = edge(img, 'Canny');
@@ -44,23 +123,11 @@ function inpainted = RemoveNet(img)
     inpainted = regionfill(img, netMask);
 end
 
-function result = Preprocess(img)
-    %Original cropping
-    I2 = imcrop(img,[100 100 1400 1500]);
-    %Modified cropping + rotated
-    %I2 = imcrop(imrotate(I, 270),[0 200 825 1200]);
-    I3 = rgb2gray(I2);
-    I4 = imadjust(I3);
-    I5 = im2uint8(I4);
-    I6 = adapthisteq(I5);
-    result = imsharpen(I6);
-end
-
-function [annotated,bw_inverted,Ibwopen] = extractFish(inpainted,fishElem,hBlobAnalysis,originalPic)
+function [annotated,bw_inverted,Ibwopen,objCentroid] = extractFish(inpainted,fishElem,hBlobAnalysis,originalPic)
     image = inpainted;
     %image = imsharpen(inpainted); % TODO: might be unnesscary
     % Thresholding
-    bw = imbinarize(image,"adaptive","Sensitivity",0.65);
+    bw = imbinarize(image,"adaptive","Sensitivity",0.7);
     bw_inverted = ~bw;
     % Remove disturbances
     Ibwopen = imopen(bw_inverted,fishElem);
